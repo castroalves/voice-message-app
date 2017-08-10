77570af6af14be9cf821737a10e583dfe7f4cc1c
Cadu de Castro Alves <cadu@deskovery.net>
2017-08-10 16:16:08 -0300

    Moved to Express

---
+++ README.md
@@ -0,0 +1,5 @@
+# WeDeploy Node.js Tutorial
+
+This template will help you through the process of deploying a Node.js project on WeDeploy.
+
+> [Visit the tutorial to learn how to start using Node.js with WeDeploy.](https://wedeploy.com/tutorials/nodejs/)
---
+++ index.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+var bodyParser = require('body-parser');
+var app = express();
+
+var accountSid = 'AC5f2391649a6b0d4c7819a336efb47041';
+var authToken = '46ab3eb0b47adc2d4357981557dd9c3d';
+var client = require('twilio')(accountSid, authToken);
+
+app.use(morgan('combined'));
+
+app.use(bodyParser.text({ type: 'text/html' }));
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.post('/call', urlencodedParser, function(req, res) {
+
+	var fromNumber = req.body.from;
+	var toNumber = req.body.to;
+
+	client.calls.create({
+		url: 'https://castroalves.github.io/voice.xml',
+		from: '+55' + fromNumber,
+		to: '+55' + toNumber
+	}, function(err, call) {
+
+		if( call.sid ) {
+			res.send('Mensagem enviada com sucesso!');
+		} else {
+			res.send('Houve um erro ao enviar a mensagem!');
+		}
+
+		res.status(200).end();
+
+		console.log(err);
+		console.log(call);
+	});
+
+});
+
+app.listen(80, function () {
+  console.log('Listening on port 80');
+});
