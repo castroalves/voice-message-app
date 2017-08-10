e9d4d890204b652a14a64d09e2b10c30d5a63d3e
Cadu de Castro Alves <cadu@deskovery.net>
2017-08-10 17:30:11 -0300

    Fixed play route method

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.get('/play', xmlParser, function (req, res) {
+app.post('/play', xmlParser, function (req, res) {
 	res.sendFile(path.join(__dirname + '/public/assets/voice.xml'));
 });
 
