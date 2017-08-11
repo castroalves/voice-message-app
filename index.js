f3bf412607dfdc21bff95432e0f3dcaa7ecdefc6
Cadu de Castro Alves <cadu@deskovery.net>
2017-08-11 20:50:20 -0300

    - General code cleanup - Added support to .env variables - Added Twilio Voice API integration - Added JSON responses - Não sei mais o que colocar aqui =)

--- index.js
+++ index.js
@@ -5,15 +5,14 @@ var bodyParser = require('body-parser');
 var request = require('request');
 var app = express();
 
-// var accountSid = '';
-// var authToken = '';
-// var client = require('twilio')(accountSid, authToken);
+require('dotenv').config();
 
-var port = 80;
+var accountSid = process.env.TWILIO_ACCOUNT_SID;
+var authToken = process.env.TWILIO_AUTH_TOKEN;
+var client = require('twilio')(accountSid, authToken);
 
-// Total Voice API Settings
-var totalVoiceApi = 'https://api.totalvoice.com.br/composto';
-var totalVoiceAuthToken = '';
+var port = 3000;
+var debug = true;
 
 app.use(morgan('combined'));
 
@@ -23,74 +22,43 @@ app.use(bodyParser.text({ type: 'text/html' }));
 
 var xmlParser = bodyParser.text({ type: 'text/xml' });
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
-var jsonParser = bodyParser.json();
 
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname + '/public/index.html'));
-});
-
-app.post('/call', urlencodedParser, function (req, res) {
-
-	if( ! req.body ) return res.sendStatus(400);
-
-	var options = {
-		url: totalVoiceApi,
-		headers: {
-			'Access-Token': totalVoiceAuthToken
-		},
-		form: {
-			numero_destino: req.body.to,
-			dados: [
-				{
-					acao: 'audio',
-					acao_dados: {
-						url_audio: 'https://github.com/castroalves/gemidao-do-zap-twilio/blob/master/public/assets/gemidao.mp3?raw=true'
-					}
-				}
-			],
-			bina: req.body.from
-		},
-		json: true
-	};
-
-	request.post(options, function(error, response, body) {
-		if( ! error && response.statusCode == 200 ) {
-			res.end('Mensagem enviada com sucesso!');
-		} else {
-			res.end('Erro ao enviar a mensagem!');
-		}
-	});
-
 });
 
 app.post('/play', xmlParser, function (req, res) {
 	res.sendFile(path.join(__dirname + '/public/assets/voice.xml'));
 });
 
-// app.post('/call', urlencodedParser, function(req, res) {
+app.post('/call', urlencodedParser, function(req, res) {
 
-// 	var fromNumber = req.body.from;
-// 	var toNumber = req.body.to;
+	if( req.body && req.body.from && req.body.to ) {
 
-// 	client.calls.create({
-// 		url: 'https://app-gemidao.wedeploy.io/play',
-// 		from: '+55' + fromNumber,
-// 		to: '+55' + toNumber
-// 	}, function(err, call) {
+		var fromNumber = req.body.from;
+		var toNumber = req.body.to;
 
-// 		if( call.sid ) {
-// 			res.send('Mensagem enviada com sucesso!');
-// 		} else {
-// 			res.send('Houve um erro ao enviar a mensagem!');
-// 		}
+		client.calls.create({
+			url: 'https://app-gemidao.wedeploy.io/play',
+			from: '+55' + fromNumber,
+			to: '+55' + toNumber
+		}, function(err, call) {
 
-// 		res.status(200).end();
+			res.json({
+				success: true, 
+				message: 'Sua mensagem foi enviada com sucesso!'
+			});
 
-// 		console.log(err);
-// 		console.log(call);
-// 	});
+		});
 
-// });
+	} else {
+		res.json({
+			success: false, 
+			message: 'Você deve preencher os dois campos.'
+		});
+	}
+
+});
 
 app.listen(port, function () {
   console.log('Listening on port ' + port);
